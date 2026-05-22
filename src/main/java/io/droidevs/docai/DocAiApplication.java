8456ea04b5aede83da6b4a7954893f6870cdd075
Mouad OUMOUS <mouadoumous@gmail.com>
2026-05-22 11:31:37 +0200

    Initial commit

---
+++ src/main/java/io/droidevs/docai/DocAiApplication.java
@@ -0,0 +1,13 @@
+package io.droidevs.docai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocAiApplication.class, args);
+	}
+
+}

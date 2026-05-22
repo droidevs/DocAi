bf1c439b12590f7b6179a5b41a3623fc147e5558
Mouad OUMOUS <mouadoumous@gmail.com>
2026-05-22 11:41:35 +0200

    fix

--- src/test/java/io/droidevs/docai/DocAiApplicationTests.java
+++ src/test/java/io/droidevs/docai/DocAiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DocAiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
